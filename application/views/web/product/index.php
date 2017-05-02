233272a6a89262cfc203acfa726970b5e9cdb91b
chiuko <q8156697@gmail.com>
2017-05-02 13:36:48 +0800

    rm index.php for ubuntu

--- application/views/web/product/index.php
+++ application/views/web/product/index.php
@@ -1,7 +1,7 @@
 <div id="page-header"></div>
 <div id="page-container">
 	<div class="page-body">
-		<a href="keyIn">新增進貨</a>
+		<a href="<?php echo base_url(); ?>/product/keyIn">新增進貨</a>
 		<a href="warehouse-set.html">倉庫設定</a>
 		<a href="warehouse-list.html">倉庫</a>
 		<a href="warehouse-in.html">庫存</a>
