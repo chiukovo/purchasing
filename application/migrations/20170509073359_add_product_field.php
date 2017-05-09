3af0e37426abd04483bc395296ea2539e31b0180
chiuko <q8156697@gmail.com>
2017-05-09 16:49:10 +0800

    fix

--- application/migrations/20170509073359_add_product_field.php
+++ application/migrations/20170509073359_add_product_field.php
@@ -11,9 +11,9 @@ class AddProductField extends AbstractMigration
     {
         $this->query("
             ALTER TABLE `product`
-            ADD COLUMN `warehouse` INT(11) NULL DEFAULT NULL AFTER `code`,
-            ADD COLUMN `receiver` INT(11) NULL DEFAULT NULL AFTER `warehouse`,
-            ADD COLUMN `freight` INT(11) NULL DEFAULT NULL AFTER `receiver`,
+            ADD COLUMN `warehouse` VARCHAR(200) NULL DEFAULT NULL AFTER `code`,
+            ADD COLUMN `receiver` VARCHAR(200) NULL DEFAULT NULL AFTER `warehouse`,
+            ADD COLUMN `freight` VARCHAR(200) NULL DEFAULT NULL AFTER `receiver`,
             ADD COLUMN `tracking_code` VARCHAR(200) NULL DEFAULT NULL AFTER `freight`;
         ");
     }
